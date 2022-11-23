0f5777f996c5c97c0d021d4483d9b1001916ecd8
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-11-22 17:43:06 -0800

    adding some data types

---
+++ assets/scripts/app.js
@@ -0,0 +1,8 @@
+const defaultResult = 0;
+let currentResult = 0;
+
+currentResult = currentResult + 10;
+
+let calculationDescription = `${defaultResult}  + 10`;
+
+outputResult(currentResult, calculationDescription);
